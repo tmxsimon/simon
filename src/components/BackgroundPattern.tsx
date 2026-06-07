81fe472d8f1fbc7dde33635baac2cb2f9a8faf2f
tmxsimon <tm.simon@proton.me>
2026-06-07 12:10:25 +0200

    fix: bg path

--- src/components/BackgroundPattern.tsx
+++ src/components/BackgroundPattern.tsx
@@ -7,8 +7,8 @@ const BackgroundPattern = () => {
     <div
       className={
         theme === "dark"
-          ? "pointer-events-none absolute inset-0 bg-[url('simon/bg/bg-dark.svg')] opacity-3"
-          : "pointer-events-none absolute inset-0 bg-[url('simon/bg/bg-light.svg')] opacity-6"
+          ? "pointer-events-none absolute inset-0 bg-[url('/simon/bg/bg-dark.svg')] opacity-3"
+          : "pointer-events-none absolute inset-0 bg-[url('/simon/bg/bg-light.svg')] opacity-6"
       }
     />
   );
